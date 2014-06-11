d93da987e252602c79e3aa3266e3d0caf5329e59
Lars <larspeter94@gmail.com>
2014-06-11 13:48:42 +0200

    screw you guys - i'm going home
    - like douchebag would say it

--- ASE/src/program/ITerminalConnection.java
+++ ASE/src/program/ITerminalConnection.java
@@ -1,5 +1,9 @@
 package program;
 
-public class ITerminalConnection {
-
+public interface ITerminalConnection {
+	boolean terminalConnect(String host, String port);
+	void terminalDisconnect();
+	String getTerminalResponse(String output);
+	String getHost();
+	String getPort();
 }
