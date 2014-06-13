f61ab0a053605325b3ed5311fac39bcf6772bdf6
Freshyz <Freshyz@10.16.163.120>
2014-06-13 12:37:06 +0200

    d

--- ASE/src/Users/OperatorDTO.java
+++ ASE/src/Users/OperatorDTO.java
@@ -11,7 +11,7 @@ public class OperatorDTO implements IOperatorDTO {
 		String opr;
 		String password;
 		
-		public Operator(int id, String name,String opr, String password) {
+		public Operator(int id, String name, String opr, String password) {
 			super();
 			this.id = id;
 			this.name = name;
@@ -26,35 +26,26 @@ public class OperatorDTO implements IOperatorDTO {
 			this.password = password;
 		}
 	}
-	
-	public String getName(String opr) {
+	public String getName(int id) {
 		for (int i = 0; i < oprs.size(); i++)
-			if(oprs.get(i).opr.equals(opr))
+			if(oprs.get(i).opr.equals(id))
 				return oprs.get(i).name;
 		return null;
 	}
 	
 
-	public int getOprId(String opr) {
+	public int getOprId(int id) {
 		for (int i = 0; i < oprs.size(); i++)
-			if(oprs.get(i).opr.equals(opr))
+			if(oprs.get(i).opr.equals(id))
 				return oprs.get(i).id;
 		return -1;
 	}
 	
-	public String getPassword(String opr) {
+	public String getPassword(int id) {
 		for (int i = 0; i < oprs.size(); i++)
-			if(oprs.get(i).opr.equals(opr))
+			if(oprs.get(i).opr.equals(id))
 				return oprs.get(i).password;
 		return null;
 	}
 	
-		
-	private int getIndex(String opr) {
-		for(int i = 0; i < oprs.size(); i++)
-			if(oprs.get(i).opr.equals(opr))
-				return i;
-		
-		return -1;
-	}
 }
