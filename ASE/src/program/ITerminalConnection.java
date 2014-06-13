7ca2b19f4bc2630ad98cb9520a6bd7ba2a262116
Lars <larspeter94@gmail.com>
2014-06-13 11:03:41 +0200

    ITerminalCon + TerminalCon slettet

--- ASE/src/program/ITerminalConnection.java
+++
@@ -1,9 +0,0 @@
-package program;
-
-public interface ITerminalConnection {
-	boolean terminalConnect(String host, String port);
-	void terminalDisconnect();
-	String getTerminalResponse(String output);
-	String getHost();
-	String getPort();
-}
